864f6a1c0054969a1d19371ee70f4000ceb5be04
Harper Bellows <harper.bellows@gmail.com>
2018-08-28 21:09:43 -0600

    add things

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added!"
+      redirect_to categories_path
+    else
+      flash[:failure] = 'This category name already exists!'
+      redirect_to new_category_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "#{@category.title} added!"
+      redirect_to categories_path
+    else
+      flash[:failure] = 'This category name already exists!'
+      redirect_to edit_category_path(@category)
+    end
+  end
+
+  def destroy
+    @category.destroy
+
+    redirect_to categories_path
+  end
+
+  private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:title)
+    end
+end
--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -1,29 +1,38 @@
 require 'rails_helper'
 
 describe "User creates a new job" do
-  scenario "a user can create a new job" do
-    category = Category.create!(title: "Category 1")
+  it "should allow user to create a new job" do
     company = Company.create!(name: "ESPN")
-
+    category = Category.create!(title: "Category 1")
     visit jobs_path
 
-    click_link 'Add New Job'
+    click_link "Add New Job"
 
     expect(current_path).to eq(new_job_path)
 
-    select "Test Category", from: "job[category_id]"
+    select "Category 1", from: "job[category_id]"
     fill_in "job[title]", with: "Developer"
     select "ESPN", from: "job[company_id]"
     fill_in "job[description]", with: "So fun!"
-    fill_in "job[level_of_interest]", with: 80
+    select "4 stars", from: "job[level_of_interest]"
     fill_in "job[city]", with: "Denver"
 
     click_button "Create"
 
-    expect(current_path).to eq("/jobs/#{Job.last.id}")
+    expect(current_path).to eq(job_path(Job.last))
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
-    expect(page).to have_content("80")
+    expect(page).to have_content("4 stars")
     expect(page).to have_content("Denver")
+  end
+  it 'has a link to create new category' do
+    company = Company.create!(name: 'ESPN')
+    category = Category.create!(title: 'Test Category')
+
+    visit new_job_path
+
+    click_link 'Add New Category'
+
+    expect(current_path).to eq(new_category_path)
   end
 end
